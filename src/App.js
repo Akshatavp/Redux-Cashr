38d84043c127c94a8ab30cab013c2e9e19040747
Dhanushchandra <Dhanushbharath9@gmail.com>
2024-04-19 00:10:43 +0530

    added inventory edit modal and table.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-
-import "./App.css"
+import "./App.css";
+import { Modal } from "react-bootstrap";
 import { FaCashRegister } from "react-icons/fa6";
 import { MdOutlineInventory } from "react-icons/md";
 import { AiOutlineStock } from "react-icons/ai";
@@ -12,64 +12,137 @@ import Settings from "./components/Settings";
 function App() {
   return (
     <div className="App">
+      <div
+        style={{
+          height: "80px",
+          backgroundColor: "green",
+          marginBottom: "20px",
+        }}
+      ></div>
 
-    <div style={{
-      height: "80px",
-      backgroundColor: "green",
-      marginBottom: "20px"
-    }}>
-
-    </div>
-
-
-     <div className="d-flex align-items-start">
-  <div className="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-    <button className="nav-link active" style={{
-      border: "1px solid black"
-    }} id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">
-     <div className="conrtainer">
-      <div className="row">
-      <FaCashRegister/><span>Billing</span> </div></div>
-    </button>
-    <button className="nav-link " style={{
-      border: "1px solid black"
-    }} id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">
-     <div className="conrtainer">
-      <div className="row">
-      <MdOutlineInventory/><span>INVENTORY</span> </div></div>
-    </button>
-    <button className="nav-link " style={{
-      border: "1px solid black"
-    }} id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">
-     <div className="conrtainer">
-      <div className="row">
-      <AiOutlineStock/><span>SALES</span> </div></div>
-    </button>
-    <button className="nav-link " style={{
-      border: "1px solid black"
-    }} id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">
-     <div className="conrtainer">
-      <div className="row">
-      <IoSettingsSharp/><span>SETTINGS</span> </div></div>
-    </button>
-   
-  </div>
-  <div className="tab-content" id="v-pills-tabContent">
-    <div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-      <Billing/>
-    </div>
-    <div className="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
-      <Inventory/>
-    </div>
-    <div className="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
-      <Sales/>
-    </div>
-    <div className="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
-      <Settings/>
-    </div>
-  </div>
-</div>
-
+      <div className="d-flex align-items-start">
+        <div
+          className="nav flex-column nav-pills me-3"
+          id="v-pills-tab"
+          role="tablist"
+          aria-orientation="vertical"
+        >
+          <button
+            className="nav-link active"
+            style={{
+              border: "1px solid black",
+            }}
+            id="v-pills-home-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#v-pills-home"
+            type="button"
+            role="tab"
+            aria-controls="v-pills-home"
+            aria-selected="true"
+          >
+            <div className="conrtainer">
+              <div className="row">
+                <FaCashRegister />
+                <span>Billing</span>{" "}
+              </div>
+            </div>
+          </button>
+          <button
+            className="nav-link "
+            style={{
+              border: "1px solid black",
+            }}
+            id="v-pills-home-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#v-pills-profile"
+            type="button"
+            role="tab"
+            aria-controls="v-pills-home"
+            aria-selected="true"
+          >
+            <div className="conrtainer">
+              <div className="row">
+                <MdOutlineInventory />
+                <span>INVENTORY</span>{" "}
+              </div>
+            </div>
+          </button>
+          <button
+            className="nav-link "
+            style={{
+              border: "1px solid black",
+            }}
+            id="v-pills-home-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#v-pills-messages"
+            type="button"
+            role="tab"
+            aria-controls="v-pills-home"
+            aria-selected="true"
+          >
+            <div className="conrtainer">
+              <div className="row">
+                <AiOutlineStock />
+                <span>SALES</span>{" "}
+              </div>
+            </div>
+          </button>
+          <button
+            className="nav-link "
+            style={{
+              border: "1px solid black",
+            }}
+            id="v-pills-home-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#v-pills-settings"
+            type="button"
+            role="tab"
+            aria-controls="v-pills-home"
+            aria-selected="true"
+          >
+            <div className="conrtainer">
+              <div className="row">
+                <IoSettingsSharp />
+                <span>SETTINGS</span>{" "}
+              </div>
+            </div>
+          </button>
+        </div>
+        <div className="tab-content" id="v-pills-tabContent">
+          <div
+            className="tab-pane fade show active"
+            id="v-pills-home"
+            role="tabpanel"
+            aria-labelledby="v-pills-home-tab"
+          >
+            <Billing />
+          </div>
+          <div
+            className="tab-pane fade"
+            id="v-pills-profile"
+            role="tabpanel"
+            aria-labelledby="v-pills-profile-tab"
+          >
+            <Inventory />
+          </div>
+          <div
+            className="tab-pane fade"
+            id="v-pills-messages"
+            role="tabpanel"
+            aria-labelledby="v-pills-messages-tab"
+          >
+            <Sales />
+          </div>
+          <div
+            className="tab-pane fade"
+            id="v-pills-settings"
+            role="tabpanel"
+            aria-labelledby="v-pills-settings-tab"
+          >
+            <Settings />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Inventory/index.js
+++ src/components/Inventory/index.js
@@ -1,9 +1,147 @@
-const Inventory = () => {
-    return (
-        <>
-       <h1>INVENTORY</h1> 
-        </>
-    )
-}
+import { useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
 
-export default Inventory
+const Inventory = () => {
+  const [edit, setEdit] = useState({
+    status: false,
+    name: "",
+    qty: "",
+    price: "",
+    category: "",
+    barcode: "",
+    index: null,
+  });
+
+  const handleClose = () => setEdit({ ...edit, status: false });
+
+  const saveproductchange = () => {
+    const updatedProducts = [...products];
+
+    console.log(updatedProducts[edit.index]);
+    updatedProducts[edit.index] = {
+      name: edit.name,
+      qty: edit.qty,
+      price: edit.price,
+      category: edit.category,
+      barcode: edit.barcode,
+    };
+
+    setProducts(updatedProducts);
+    handleClose();
+  };
+
+  const [products, setProducts] = useState([
+    {
+      name: "Milk",
+      qty: 65,
+      price: 1200,
+      category: "daily",
+      barcode: "675GBR",
+    },
+    {
+      name: "Milk",
+      qty: 65,
+      price: 1200,
+      category: "daily",
+      barcode: "675GBR",
+    },
+    {
+      name: "Milk",
+      qty: 65,
+      price: 1200,
+      category: "daily",
+      barcode: "675GBR",
+    },
+    {
+      name: "Milk",
+      qty: 65,
+      price: 1200,
+      category: "daily",
+      barcode: "675GBR",
+    },
+    {
+      name: "Milk",
+      qty: 65,
+      price: 1200,
+      category: "daily",
+      barcode: "675GBR",
+    },
+  ]);
+
+  const Modell = () => {
+    return (
+      <>
+        <Modal show={edit.status} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit Product</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Control defaultValue={edit.name}></Form.Control>
+            <Form.Control defaultValue={edit.qty}></Form.Control>
+            <Form.Control defaultValue={edit.price}></Form.Control>
+            <Form.Control defaultValue={edit.category}></Form.Control>
+            <Form.Control defaultValue={edit.barcode}></Form.Control>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={saveproductchange}>
+              Save changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  };
+
+  console.log(edit.name);
+
+  return (
+    <>
+      <Modell />
+      <table class="table table-striped in-table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Quantity</th>
+            <th scope="col">Price</th>
+            <th scope="col">Category</th>
+            <th scope="col">Barcode</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((x, index) => (
+            <tr key={index}>
+              <td>{x.name}</td>
+              <td>{x.qty}</td>
+              <td>{x.price}</td>
+              <td>{x.category}</td>
+              <td>{x.barcode}</td>
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  setEdit({
+                    status: true,
+                    name: x.name,
+                    qty: x.qty,
+                    price: x.price,
+                    category: x.category,
+                    barcode: x.barcode,
+                    index: index,
+                  });
+                }}
+              >
+                Edit
+              </button>
+              <button className="btn btn-danger">delete</button>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Inventory;
